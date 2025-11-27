7d79b29edcf459857d63b6e5c1652417eb54331b
Jessica B. Pérez <jessica26per@gmail.com>
2025-11-27 17:11:06 -0600

    Se añade el segundo constructor ya que el primero no era correcto

--- Student.java
+++ Student.java
@@ -8,23 +8,39 @@ public class Student {
 	       int year;
 	       
 	       
-	       public void printFullName(String firstName, String lastName ){
-	    	   this.firstName = firstName.toUpperCase();
-	           this.lastName =  lastName.toUpperCase();
-	           this.registration = registration;
-	           this.grade = grade;
-	           this.year = year;
+	       public Student(String firstName, String lastName, int registration, int grade, int year) {
+	    	    this.firstName = firstName;
+	    	    this.lastName = lastName;
+	    	    this.registration = registration;
+	    	    this.grade = grade;
+	    	    this.year = year;
+	       
+}//class student    
+	       
+	       public Student(String firstName, String lastName, int registration) {
+	    	    this.firstName = firstName;
+	    	    this.lastName = lastName;
+	    	    this.registration = registration;
+	    	    this.grade = 0; 
+	    	    this.year = 1;
+	       
+	       }// constructor 2
+	       
+	       
+	       public void printFullName(){
+	    	    //TODO implement 
+	    	   
 	    		}
 
 	       public void isApproved(){
 	    	     //TODO implement: should return true if grade >= 60
 	    		 }
 
-	       public int changeYearIfApproved(){
+	    public int changeYearIfApproved(){
 	    	     //TODO implement: the student should advance to the next year if he/she grade is >= 60
 	    	     // Make year = year + 1, and print "Congragulations" if the student has been approved
 	    	     return 0;
-	    	     }
+	    }
 	       
-}
+
 
