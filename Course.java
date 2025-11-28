bdd18a2ff5238e025a73edcf86ae1874b103a6ff
Jessica B. Pérez <jessica26per@gmail.com>
2025-11-27 21:35:59 -0600

    Se crean los metodos de curse

--- Course.java
+++ Course.java
@@ -15,5 +15,35 @@ public class Course {
         this.enrolledStudents = new ArrayList<>(); 
     }
     
+    public void enroll(Student student) {
+        enrolledStudents.add(student);
+        System.out.println(student.firstName + " ha sido inscrito en el curso de " + courseName + ".");
+    }
+
+    public void unEnroll(Student student) {
+        boolean removed = enrolledStudents.remove(student);
+        
+        if (removed) {
+            System.out.println(student.firstName + " ha sido dado de baja.");
+        } else {
+            System.out.println("ERROR: " + student.firstName + " no se encuentra en este curso.");
+        }
+    }
+
+    public int countStudents() {
+        return enrolledStudents.size(); 
+    }
+
+    public int bestGrade() {
+        int maxGrade = 0; 
+
+        for (Student s : enrolledStudents) {
+            if (s.grade > maxGrade) {
+                maxGrade = s.grade;
+            }
+        }
+        return maxGrade;
+    }
+    
     
 }
